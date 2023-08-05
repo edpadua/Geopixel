b2cc4b5ff722b2fb084d8b801ede372be1ae72df
edpadua <ed.padua@gmail.com>
2023-08-04 21:30:42 -0300

    Update 2
    Update 2

--- geopixel/src/Components/Ball/index.jsx
+++ geopixel/src/Components/Ball/index.jsx
@@ -1,31 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import styles from "./Ball.module.css"
 
 
 function Ball() {
     const [isShown, setIsShown] = useState(
-        {x:0,y:0}
+        { x: 0, y: 0 }
     );
+
+    const [height, setHeight] = useState(0);
+    const [width, setWidth] = useState(0);
+
+    const ref = useRef(null);
+
+    const getListSize = () => {
+        const newWidth = ref.current.clientWidth;
+        setWidth(newWidth);
+    
+        const newHeight = ref.current.clientHeight;
+        setHeight(newHeight);
+      };
+
+      useEffect(() => {
+        getListSize();
+      }, []);
+
+      useEffect(() => {
+        window.addEventListener("resize", getListSize);
+      }, []);
+
 
     function randomNumberInRange(min, max) {
         // get number between min (inclusive) and max (inclusive)
         return Math.floor(Math.random() * (max - min + 1)) + min;
-      }
+    }
 
     return (
-        <div className={styles.container}>
+        <div ref={ref} className={styles.container}>
             <span className={styles.ball}
-                onMouseEnter={() => setIsShown({x:randomNumberInRange(-700,700),y:randomNumberInRange(-1000,1000)})}
-                onMouseLeave={() => setIsShown({x:0,y:0})}
+                onMouseEnter={() => setIsShown({ x: randomNumberInRange(-(height/2),(height/2)), y: randomNumberInRange(-(width/2),(width/2)) })}
+                onMouseLeave={() => setIsShown({ x: 0, y: 0 })}
                 style={{
-                    transform: `translateY(${isShown.y}%) translateX(${isShown.x}%)`
-                    
-                  }}
+                    transform: `translateY(${isShown.y}px) translateX(${isShown.x}px)`
+
+                }}
             >
-        
+
 
             </span>
+          
         </div>
     )
 }
